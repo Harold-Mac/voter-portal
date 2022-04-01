fc60ac6f6f5ab264eaacc903449b3379c8bd6392
Harold Andrei Macalisang <57225901+Harold-Mac@users.noreply.github.com>
2022-04-01 15:00:29 +0800

    Added forms.py

---
+++ portalsite/voters/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class CreateUserForm(UserCreationForm):
+	first_name = forms.CharField(max_length=30, required=False, label="First Name")
+	last_name = forms.CharField(max_length=30, required=False)
+
+
+	class Meta:
+		model = User
+
+		fields = ['first_name', 'last_name', 'email' ,'password1', 'password2']
+
+	def save(self, commit=True):
+		user = super(UserCreationForm, self).save(commit=False)
+		user.set_password(self.cleaned_data["password1"])
+		user.username = '{}.{}'.format(self.cleaned_data['first_name'].replace(" ", "").lower(),self.cleaned_data['last_name'].replace(" ", "").lower())
+
+		if commit:
+			user.save()
+		return user
