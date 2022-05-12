b1c90b0b8e4e95d25d4a9c61da3dcbe3cd63d0b1
anagramiccoder <jdvidad31@gmail.com>
2022-05-12 23:11:22 +0800

    fixed graph in rep Scheduling

--- portalsite/voters/views.py
+++ portalsite/voters/views.py
@@ -223,5 +223,15 @@ def repSchedview(request,*args,**kwargs):
     user=request.user
     if not user.is_authenticated:
         return redirect('Home')
+    prec=Repre.objects.filter(user=user)[0].pNum
+    schedules=["7:00 AM","7:30 AM", "8:00 AM","8:30 AM", "9:00 AM","9:30 AM", "10:00 AM","10:30 AM", "11:00 AM","11:30 AM", "12:00 PM","12:30 PM", "1:00 PM","1:30 PM", "2:00 PM","2:30 PM", "3:00 PM","3:30 PM", "4:00 PM","4:30 PM", "5:00 PM","5:30 PM", "6:00 PM","6:30 PM","7:00 PM"]
+    reprevoters=Repre.objects.filter(pNum=prec,scheduled=True).values("scheduleddate")
+    vvoters=Voter.objects.filter(pNum=prec,scheduled=True).values("scheduleddate")
+    scheds=[]
+    print(reprevoters,vvoters)
+    for i in schedules:
+        tots=reprevoters.filter(scheduleddate__startswith=i).count()+vvoters.filter(scheduleddate__startswith=i).count()
+        scheds.append(tots)
+    print(scheds)
     full_name=user.first_name+" "+user.last_name
-    return render(request,"repSched.html",{'name':full_name})
+    return render(request,"repSched.html",{'name':full_name, "data":scheds,})
